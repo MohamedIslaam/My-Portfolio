8701bdb7d7afad812c7e75f4dc745c18bd2f9067
Mohamed Islaam K A <mohamedislaam87@gmail.com>
2024-10-12 12:02:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var btn=document.getElementById("btn");
+btn.addEventListener('click',function(){
+    alert("This form does not send any data. If you wish to contact me, please reach out via email at mohamedislaam87@gmail.com.");
+});
+
+const nav=document.getElementById("open");
+const clos=document.getElementById("close");
+const side=document.getElementById("side-nav");
+const lis=document.getElementsByClassName("lis");
+
+nav.addEventListener('click',()=>{
+    side.style.display="inline";
+});
+
+clos.addEventListener('click',()=>{
+    side.style.display="none";
+});
+
+for (let i = 0; i < lis.length; i++) {
+    lis[i].addEventListener('click', () => {
+        side.style.display = "none";
+    });
+}
